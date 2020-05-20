ed6cc805164ed17b6ec98aaf758daec4e9606588
wwmarkositrawe <wungielq@gmail.com>
2020-05-20 20:49:42 +0200

    engine with SELECT clause to fix

--- backend.py
+++ backend.py
@@ -1,33 +1,61 @@
 import sqlite3
+import os
 
-try:
-    sqliteConnection = sqlite3.connect('journeys.db')
 
-    cursor = sqliteConnection.cursor()
+DB_FILENAME = 'journeys.db'
 
-    with open(r'E:\autobusy\autobusy-backend\queries.sql', 'r') as queries:
-        sql_script = queries.read()
 
-    # cursor.executescript(sql_script)
+def clear_old_db_file():
+    if os.path.exists(DB_FILENAME):
+        os.remove(DB_FILENAME)
 
-    print("Scripts successfully executed")
 
+def read_init_script():
+    with open(r'queries.sql', 'r') as queries:
+        return queries.read()
+
+
+def init_database(sqliteConnection):
+    try:
+        init_script = read_init_script()
+
+        cursor = sqliteConnection.cursor()
+        cursor.executescript(init_script)        
+        sqliteConnection.commit()
+    except Exception as e:
+        print('There was an error during the db initializaiton!', e)
+        # code != 0 means failure
+        exit(1)
+
+
+def fetch_hours(cursor, stop_from_name, stop_to_name):
     cursor.execute(
-        "")
+        '''SELECT T.TIME FROM TIMETABLE T
+             JOIN STOP S1 ON T.STOP_ID = S1.ID 
+             JOIN STOP S2 ON S2.ROUTE_ID = S1.ROUTE_ID 
+             WHERE S1.STOP_NAME = ? 
+             AND S2.STOP_NAME = ? ORDER BY T.TIME ASC''', 
+        (stop_from_name, stop_to_name))  # '?'s protect from sql injection.
+    return cursor.fetchall()
 
-    rows = cursor.fetchall()
 
-    for row in rows:
-        print(row[0])
+def display_hours(hours):
+    print('Departures: ' + ', '.join(map(lambda h: h[0], hours)))
+    
 
-    sqliteConnection.commit()
 
-    cursor.close()
+if __name__ == '__main__':  
+    try:
+        clear_old_db_file()
 
-except sqlite3.Error as error:
-    print("Error while connecting to sqlite", error)
-finally:
-    if (sqliteConnection):
-        sqliteConnection.close()
-        print("The SQLite connection is closed")
+        sqliteConnection = sqlite3.connect(DB_FILENAME)
 
+        init_database(sqliteConnection)
+
+        cursor = sqliteConnection.cursor()
+        hours = fetch_hours(cursor, 'Smolajny', 'Dobre Miasto')
+
+        display_hours(hours)
+    finally:
+        if (sqliteConnection):
+            sqliteConnection.close()
--- queries.sql
+++ queries.sql
@@ -1,36 +1,22 @@
-CREATE TABLE ROUTE (
-ROUTE_ID INTEGER PRIMARY KEY AUTOINCREMENT);
+-- safe option - checking foreign keys
+PRAGMA foreign_keys=1;
 
 CREATE TABLE STOP (
-STOP_ID INTEGER PRIMARY KEY AUTOINCREMENT,
-STOP_NAME STRING,
-ROUTE_ID INTEGER,
-FOREIGN KEY (ROUTE_ID) REFERENCES ROUTE (ROUTE_ID));
+ID INTEGER PRIMARY KEY,
+STOP_NAME TEXT NOT NULL,
+ROUTE_ID INTEGER NOT NULL-- if two stops have the same number, they are on the same route
+);
 
 CREATE TABLE TIMETABLE (
-TIMETABLE_ID INTEGER PRIMARY KEY AUTOINCREMENT,
-TIME STRING,
-ROUTE_ID INTEGER,
-FOREIGN KEY (ROUTE_ID) REFERENCES ROUTE (ROUTE_ID));
+ID INTEGER PRIMARY KEY, 
+TIME TEXT NOT NULL,
+STOP_ID INTEGER NOT NULL,
+FOREIGN KEY (STOP_ID) REFERENCES STOP (ID)
+);
 
-INSERT INTO ROUTE VALUES
-(1),
-(2),
-(3),
-(4),
-(5),
-(6),
-(7),
-(8),
-(9),
-(10),
-(11),
-(12);
--- (13),
--- (14),
--- (15);
 
 INSERT INTO STOP VALUES
+--towards Olsztyn
 (1,'Lidzbark Warmiński', 1),
 (2, 'Smolajny', 1),
 (3, 'Dobre Miasto', 1),
@@ -45,52 +31,91 @@ INSERT INTO STOP VALUES
 (12, 'Smolajny', 5),
 (13, 'Olsztyn', 5),
 (14, 'Dobre Miasto', 6),
-(15, 'Olsztyn', 6);
+(15, 'Olsztyn', 6),
+(16, 'Smolajny', 7),
+(17, 'Dobre Miasto', 7),
+(18, 'Olsztyn', 7);
+
+--towards LW
+-- (19, 'Olsztyn', 8),
+-- (20, 'Dobre Miasto', 8),
+-- (21, 'Smolajny', 8),
+-- (22, 'Lidzbark Warmiński', 8),
+-- (23, 'Dobre Miasto', 9),
+-- (24, 'Smolajny', 9),
+-- (25, 'Lidzbark Warmiński', 9),
+-- (26, 'Smolajny', 10),
+-- (27, 'Lidzbark Warmiński', 10),
+-- (28, 'Dobre Miasto', 11),
+-- (29, 'Smolajny', 11),
+-- (30, 'Olsztyn', 12),
+-- (31, 'Smolajny', 12),
+-- (32, 'Olsztyn', 13),
+-- (33, 'Dobre Miasto', 13),
+-- (34, 'Olsztyn', 14),
+-- (35, 'Dobre Miasto', 14),
+-- (36, 'Smolajny', 14);
+
 
 INSERT INTO TIMETABLE VALUES
-(1, '8:00', 1),
+--towards Olsztyn
+(1, '08:00', 1),
 (2, '12:00', 1),
 (3, '15:00', 1),
 (4, '19:00', 1),
-(5, '8:00', 2),
+(5, '08:00', 2),
 (6, '12:00', 2),
 (7, '15:00', 2),
 (8, '19:00', 2),
-(9, '8:00', 3),
+(9, '08:00', 3),
 (10, '12:00', 3),
 (11, '15:00', 3),
 (12, '19:00', 3),
-(13, '8:20', 4),
+(13, '08:20', 4),
 (14, '12:20', 4),
 (15, '15:20', 4),
 (16, '19:20', 4),
-(17, '8:20', 5),
+(17, '08:20', 5),
 (18, '12:20', 5),
 (19, '15:20', 5),
 (20, '19:20', 5),
-(21, '8:30', 6),
+(21, '08:30', 6),
 (22, '12:30', 6),
 (23, '15:30', 6),
-(24, '19:30', 6);
--- -- (, '', ),
--- -- (, '', ),
--- -- (, '', ),
--- -- (, '', ),
--- -- (, '', ),
--- -- (, '', ),
--- -- (, '', ),
--- -- (, '', ),
--- -- (, '', ),
--- -- (, '', ),
--- -- (, '', ),
--- -- (, '', ),
--- -- (, '', ),
--- -- (, '', ),
--- -- (, '', ),
--- -- (, '', ),
--- -- (, '', ),
--- -- (, '', ),
--- -- (, '', ),
--- -- (, '', ),
--- -- (, '', ),
--- -- (, '', ),
+(24, '19:30', 6),
+(25, '08:20', 7),
+(26, '12:20', 7),
+(27, '15:20', 7),
+(28, '19:20', 7);
+
+--towards LW
+-- (29, '09:00', 8),
+-- (30, '13:00', 8),
+-- (31, '17:00', 8),
+-- (32, '21:00', 8),
+-- (33, '09:30', 9),
+-- (34, '13:30', 9),
+-- (35, '17:30', 9),
+-- (36, '21:30', 9),
+-- (37, '09:40', 10),
+-- (38, '13:40', 10),
+-- (39, '17:40', 10),
+-- (40, '21:40', 10),
+-- (41, '09:30', 11),
+-- (42, '13:40', 11),
+-- (43, '17:40', 11),
+-- (44, '21:40', 11),
+-- (45, '09:40', 12),
+-- (46, '13:40', 12),
+-- (47, '17:40', 12),
+-- (48, '21:40', 12),
+-- (49, '09:30', 13),
+-- (50, '13:30', 13),
+-- (51, '17:30', 13),
+-- (52, '21:40', 13),
+-- (53, '09:00', 14),
+-- (54, '13:00', 14),
+-- (55, '17:00', 14),
+-- (56, '21:00', 14);
+
+
